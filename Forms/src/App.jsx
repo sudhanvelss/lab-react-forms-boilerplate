fe6325ddebd6905c6a4b77369be90c1451d5d2cd
sudhanvelss <sudhanssudhan83@gmail.com>
2024-02-02 17:18:16 +0530

    React Forms done

---
+++ Forms/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Forms from './components/Forms'
+
+
+function App() {
+  
+  return (
+   <>
+   <Forms/>
+   </>
+  )
+}
+
+export default App
